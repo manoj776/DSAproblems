1e90c8844dc17355b7a9560096c069c12ccaeee2
manoj776 <manojadiraju7@gmail.com>
2022-07-21 21:13:51 +0530

    ArrayList

---
+++ src/ArrayListClass.java
@@ -0,0 +1,23 @@
+
+
+import java.util.ArrayList;
+
+public class ArrayListClass {
+    public static void main (String[] args) {
+
+        ArrayList<Integer> al= new ArrayList<>();
+
+        al.ensureCapacity(100);
+
+        for(int i=1;i<=100;i++)
+        {
+            al.add(i);
+        }
+
+        System.out.println(al);
+
+
+
+
+    }
+}
