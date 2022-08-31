5b0bdee5a5ae715249f7331fb23d995d119be371
manoj776 <manojadiraju7@gmail.com>
2022-08-31 21:26:45 +0530

    Map

---
+++ src/Queue.java
@@ -0,0 +1,14 @@
+public class Queue {
+    public static void main (String[] args) {
+        // Queue<Integer> q = new LinkedList<Integer>();
+        Queue<Integer> q = new ArrayDeque<Integer>();
+
+        q.offer(10);
+        q.offer(20);
+        q.offer(30);
+
+        System.out.println(q.peek());
+        System.out.println(q.poll());
+        System.out.println(q.peek());
+    }
+}
