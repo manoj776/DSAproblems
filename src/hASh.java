6ebca7762e7a3eb434e76b0cd333a010658d18af
manoj776 <manojadiraju7@gmail.com>
2022-09-07 23:06:12 +0530

    BFS

---
+++ src/hASh.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class hASh {
+    public static void main (String[] args) {
+        int arr[] = new int[]{15, 16, 27, 27, 28, 15};
+        int n = arr.length;
+
+        System.out.println(countDistinct(arr, n));
+    }
+
+    static int countDistinct(int arr[], int n)
+    {
+        Set<Integer> hs = new HashSet<Integer>();
+        for(int i = 0; i < n; i++)
+            hs.add(arr[i]);
+
+        return hs.size();
+    }
+}
