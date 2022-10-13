e2105ebb3e5ddebf7be08c1941f69b50e6993a2d
manoj776 <manojadiraju7@gmail.com>
2022-10-13 09:35:40 +0530

    tree set example

---
+++ src/TreeSetExample.java
@@ -0,0 +1,22 @@
+class TreeSetExample {
+
+    public static void main(String[] args)
+    {
+
+        // Creating an empty TreeSet
+        TreeSet<Integer> s = new TreeSet<Integer>();
+
+        // Elements are added using add() method
+        s.add(10);
+        s.add(5);
+        s.add(2);
+        s.add(15);
+
+        // Removing 5 from TreeSet
+        s.remove(5);
+
+        // foreach way of traversal
+        for(Integer x:s)
+            System.out.print(x + " ");
+    }
+}
